23d777d0e20f5a8c5dba1a6b274cf7811796ef62
Yannik Inniger <yannik.inniger@gmail.com>
2018-05-04 12:44:38 +0200

    Change to string based values

--- src/model/dn-kvs-map.js
+++ src/model/dn-kvs-map.js
@@ -8,22 +8,22 @@ export default class DnKvsMap {
         valveAmount = parseInt(valveAmount, 10);
         if (valveAmount === 2) {
             if (this.map2Valve.size === 0) {
-                this.map2Valve.set("DN15", [1, 1.6, 2.5, 4, 6.3, 10]);
-                this.map2Valve.set("DN20", [4, 6.3, 10]);
-                this.map2Valve.set("DN25", [6.3, 10, 16]);
-                this.map2Valve.set("DN32", [10, 16, 25]);
-                this.map2Valve.set("DN40", [16, 25, 40]);
-                this.map2Valve.set("DN50", [25, 40, 63]);
+                this.map2Valve.set('DN15', ['1', '1.6', '2.5', '4', '6.3', '10']);
+                this.map2Valve.set('DN20', ['4', '6.3', '10']);
+                this.map2Valve.set('DN25', ['6.3', '10', '16']);
+                this.map2Valve.set('DN32', ['10', '16', '25']);
+                this.map2Valve.set('DN40', ['16', '25', '40']);
+                this.map2Valve.set('DN50', ['25', '40', '63']);
             }
             return this.map2Valve;
         } else if (valveAmount === 3) {
             if (this.map3Valve.size === 0) {
-                this.map3Valve.set("DN15", [1.6, 2.5, 4, 6.3]);
-                this.map3Valve.set("DN20", [4, 6.3]);
-                this.map3Valve.set("DN25", [10]);
-                this.map3Valve.set("DN32", [16]);
-                this.map3Valve.set("DN40", [25]);
-                this.map3Valve.set("DN50", [40]);
+                this.map3Valve.set('DN15', ['1.6', '2.5', '4', '6.3']);
+                this.map3Valve.set('DN20', ['4', '6.3']);
+                this.map3Valve.set('DN25', ['10']);
+                this.map3Valve.set('DN32', ['16']);
+                this.map3Valve.set('DN40', ['25']);
+                this.map3Valve.set('DN50', ['40']);
             }
             return this.map3Valve;
         }
