7079aec0df2fb4bf63893d63a07520cb57e2b957
Yannik Inniger <yannik.inniger@gmail.com>
2018-05-04 12:41:26 +0200

    Create article number service

--- src/service/article-number-service.js
+++ src/service/article-number-service.js
@@ -1,9 +1,73 @@
-import getDnKvsMap from '../model/dn-kvs-map'
+import DnKvsMap from '../model/dn-kvs-map'
 
-export const ArticleNumberService = () => {
-    const dnKvsMap = getDnKvsMap();
-    const valveMap = {2: "VKR0", 3: "BKR0"};
-    const kvsMap = {
-        1: "F350", 1.6: "F340", 2.5: "F330", 4: "F320", 6.3: "F310", 10: "F300",
+export const getArticleNumber = configuration => {
+    const valveAmount = configuration.valveAmount;
+    const dn = configuration.dn;
+
+    const dnKvsMap = DnKvsMap.getDnKvsMap(valveAmount);
+    const position = dnKvsMap.get(dn).indexOf(configuration.kvs);
+
+    let articleNumber = "";
+    if (valveAmount === '2') {
+        const twoValveFactory = TwoValveFactory();
+        articleNumber = twoValveFactory.constructArticleNumber(dn, position)
+    } else if (valveAmount === '3') {
+        const threeValveFactory = ThreeValveFactory();
+        articleNumber = threeValveFactory.constructArticleNumber(dn, position)
+    }
+
+    const dnNumber = parseInt(dn.substr(2, dn.length), 10);
+    if (dnNumber < 32) {
+        articleNumber += "\n/AKM105F120";
+    } else {
+        articleNumber += "/AKM115F120";
+    }
+    return articleNumber;
+};
+
+const TwoValveFactory = () => {
+    const prefix = "VKR0";
+    const suffix = "-FF";
+    const dn15Map = ["F350", "F340", "F330", "F320", "F310", "F300"];
+    const dnOtherMap = ["F320", "F310", "F300"];
+
+    const getMiddlePart = (dn, position) => {
+        if (dn === 'DN15') {
+            return dn15Map[position];
+        } else {
+            return dnOtherMap[position];
+        }
+    };
+    
+    return {
+        constructArticleNumber: (dn, position) => {
+            const dnNumber = dn.substr(2, dn.length);
+            return prefix + dnNumber + getMiddlePart(dn, position) + suffix;
+        }
+    }
+};
+
+const ThreeValveFactory = () => {
+    const prefix = "BKR0";
+    const suffix = "-FF";
+    const dn15Map = ["F340", "F330", "F320", "F310"];
+    const dn20Map = ["F320", "F310"];
+    const dnOther = "F310";
+
+    const getMiddlePart = (dn, position) => {
+        if (dn === 'DN15') {
+            return dn15Map[position];
+        }else if (dn === 'DN20'){
+            return dn20Map[position];
+        } else {
+            return dnOther;
+        }
+    };
+
+    return {
+        constructArticleNumber: (dn, position) => {
+            const dnNumber = dn.substr(2, dn.length);
+            return prefix + dnNumber + getMiddlePart(dn, position) + suffix;
+        }
     }
 };
