a4388144bc28f2b0af4d7f3d8958b942208c56b6
Yannik Inniger <yannik.inniger@gmail.com>
2018-05-04 17:41:48 +0200

    Create article view

--- src/components/price-display/price-display.jsx
+++ src/components/price-display/price-display.jsx
@@ -8,7 +8,8 @@ export default class PriceDisplay extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            priceService: PriceService(this.props.configObject)
+            priceService: PriceService(this.props.configObject),
+            quantity: 1
         };
         this.props.register(() => {
             this.props.configObject.registerListener(newConfig => this.updatePrice(newConfig));
@@ -25,11 +26,18 @@ export default class PriceDisplay extends React.Component {
             <div className="price-display">
                 { this.state.price ?
                     <div>
-                        <div>
-                            <label>Produkt</label>
-                            <label>{this.state.product}</label>
+                        <div className="price-row">
+                            <label id="article">{this.state.product}</label>
                         </div>
-                        <div>
+                        <div className="price-row">
+                            <label>Stück</label>
+                            <span>
+                                <i className="fas fa-plus-circle"/>
+                                <label id="quantity">{this.state.quantity}</label>
+                                <i className="fas fa-minus-circle"/>
+                            </span>
+                        </div>
+                        <div className="price-row">
                             <label>Preis</label>
                             <label>{this.state.price}</label>
                         </div>
--- src/views/order-view/order-view.jsx
+++ src/views/order-view/order-view.jsx
@@ -4,6 +4,7 @@ import InputRadio from '../../components/input-radio/input-radio'
 import DnKvsSelection from '../../components/dn-kvs-selection/dn-kvs-selection'
 import { Configuration } from '../../model/configuration'
 import PriceDisplay from "../../components/price-display/price-display";
+import './order-view.css'
 
 export default class OrderView extends React.Component {
 
@@ -38,13 +39,24 @@ export default class OrderView extends React.Component {
     render() {
         return (
             <form>
-                <InputRow name="Firma" />
-                <InputRow name="Projekt" />
-                <InputRadio name="Ventiltyp" options={this.valveOptions}
-                            model={this.state.configObject}
-                            modelProperty="valveAmount"/>
-                <DnKvsSelection configObject={this.state.configObject} register={this.registerListenersToActivate}/>
-                <PriceDisplay configObject={this.state.configObject} register={this.registerListenersToActivate}/>
+                <div id="product-section">
+                    <h2>Produkt</h2>
+                    <InputRadio name="Ventiltyp" options={this.valveOptions}
+                                model={this.state.configObject}
+                                modelProperty="valveAmount"/>
+                    <DnKvsSelection configObject={this.state.configObject} register={this.registerListenersToActivate}/>
+                    <PriceDisplay configObject={this.state.configObject} register={this.registerListenersToActivate}/>
+                </div>
+                <div id="address-section">
+                    <h2>Lieferadresse</h2>
+                    <InputRow name="Firma" />
+                    <InputRow name="Projekt" />
+                    <InputRow name="Strasse" />
+                    <div className="twin-form-row">
+                        <InputRow name="Ort" />
+                        <InputRow name="Postleitzahl" />
+                    </div>
+                </div>
             </form>
         )
     }
