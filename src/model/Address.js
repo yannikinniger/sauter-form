ba70bff65c3af0c632dc411cf941c32990e672b1
Yannik Inniger <yannik.inniger@gmail.com>
2018-05-13 02:57:04 +0200

    Create address model

--- src/model/Address.js
+++ src/model/Address.js
@@ -1,3 +1,28 @@
-export const Address = () => {
+export class Address {
 
+    requiredProperties = ['company', 'street', 'city', 'zipcode', 'email'];
+
+    constructor(addressObject) {
+        const hasRequiredProperties = this.requiredProperties.every(
+            property => Address.checkObjectProperty(property, addressObject));
+        if (!hasRequiredProperties) {
+            throw new Error("Does not have required properties")
+        } else {
+            this.address = addressObject;
+        }
+    }
+
+    getAddress() {
+        return this.address;
+    }
+
+    static checkObjectProperty(property, object) {
+        return object[property] !== "";
+    }
+}
+
+export class EmptyAddress {
+    getAddress() {
+        return {};
+    }
 }
