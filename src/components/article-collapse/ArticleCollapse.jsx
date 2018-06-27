b468173327743fbb2178ef6931b5707adf5179ca
Yannik Inniger <yannik.inniger@gmail.com>
2018-06-27 15:11:21 +0200

    Change link to normschema

--- src/components/article-collapse/ArticleCollapse.jsx
+++ src/components/article-collapse/ArticleCollapse.jsx
@@ -23,7 +23,7 @@ export default class ArticleCollapse extends React.Component {
                         <div className="price-row">
                             <label className="article">
                                 <span className="quantityDisplay">{this.props.context.getItem().quantity}x</span>
-                                Normschema <a href="/files/normschema_EQJW126.pdf" target="_blank" rel="noopener noreferrer">Link</a>
+                                Normschema <a href="https://hlk-components.ch/files/normschema_EQJW126.pdf" target="_blank" rel="noopener noreferrer">Link</a>
                             </label>
                         </div>
                     </div>
