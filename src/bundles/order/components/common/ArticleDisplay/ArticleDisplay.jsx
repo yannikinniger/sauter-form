61791bcc6dc599ed2c28a7f1b25aaafdd0e99886
Yannik Inniger <yannik.inniger@gmail.com>
2018-08-08 23:09:29 +0200

    Translate ArticleDisplay

--- src/bundles/order/components/common/ArticleDisplay/ArticleDisplay.jsx
+++ src/bundles/order/components/common/ArticleDisplay/ArticleDisplay.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import './ArticleDisplay.css'
 import {Link} from 'react-router-dom'
+import {TranslationProvider} from "../../../../translations";
 
 export default class ArticleDisplay extends React.Component {
+
+    text = TranslationProvider.translationObject.productDisplay;
 
     render() {
         return (
             <div className="wrap-collabsible">
                 <input id="collapsible" className="toggle" type="checkbox"/>
                 <label htmlFor="collapsible"
-                       className="lbl-toggle">{this.props.item.quantity}x Heizungsregler-Set</label>
+                       className="lbl-toggle">{this.props.item.quantity}x {this.text.regulatorKit}</label>
                 <div className="collapsible-content">
                     <div className="content-inner">
                         <ArticleRow description={this.props.item.articleNumber}
                                     quantity={this.props.item.quantity}/>
-                        <ArticleRow description="Heizungsregler EQJW126F001"
+                        <ArticleRow description={`${this.text.descriptionRegulator} EQJW126F001`}
                                     quantity={this.props.item.quantity}/>
-                        <ArticleRow description="Aussentemperatur-Fühler EGT301F102"
+                        <ArticleRow description={`${this.text.descriptionVL} EGT311F102`}
                                     quantity={this.props.item.quantity}/>
-                        <ArticleRow description="Vorlauftemperatur-Fühler EGT311F102"
+                        <ArticleRow description={`${this.text.descriptionAT} EGT301F102`}
                                     quantity={this.props.item.quantity}/>
                         <div className="price-row">
                             <label className="article">
                                 <span className="quantityDisplay">{this.props.item.quantity}x</span>
-                                Normschema <Link target="_blank" to="/normschema_EQJW126.pdf">Link</Link>
+                                Normschema <Link target="_blank" to="/normschema_EQJW126.pdf">{this.text.link}</Link>
                             </label>
                         </div>
                     </div>
@@ -34,12 +37,12 @@ export default class ArticleDisplay extends React.Component {
 }
 
 function ArticleRow(props) {
-    return(
-            <div className="price-row">
-                <label className="article">
-                    <span className="quantityDisplay">{props.quantity}x</span>
-                    {props.description}
-                </label>
-            </div>
-        );
+    return (
+        <div className="price-row">
+            <label className="article">
+                <span className="quantityDisplay">{props.quantity}x</span>
+                {props.description}
+            </label>
+        </div>
+    );
 }
